53a66ca15867a94af4d9e5763891a9fba6ebabf7
Martin Grellet <martin.alejandro.grellet@gmail.com>
2023-01-14 15:57:31 -0300

    Desafio 01 - Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
